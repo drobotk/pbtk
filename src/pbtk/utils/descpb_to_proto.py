ccda7077477a6da4dbc404ce2a647b3748a1809a
Dawid Krajcarz <80264606+drobotk@users.noreply.github.com>
2026-08-21 00:07:04 +0200

    Update descpb_to_proto.py

--- src/pbtk/utils/descpb_to_proto.py
+++ src/pbtk/utils/descpb_to_proto.py
@@ -3,8 +3,8 @@ from google.protobuf.descriptor_pb2 import (
     DescriptorProto,
     FieldDescriptorProto,
 )
-from google.protobuf.internal.containers import RepeatedScalarFieldContainer
 from collections import OrderedDict
+from collections.abc import Sequence
 from itertools import groupby
 
 """
@@ -129,7 +129,7 @@ def fmt_value(val, options=None, desc=None, optarr=[]):
         if isinstance(val, bool):
             val = str(val).lower()
         elif desc and desc.type == FieldDescriptorProto.TYPE_ENUM:
-            if isinstance(val, (list, RepeatedScalarFieldContainer)):
+            if isinstance(val, Sequence):
                 val = ', '.join(
                     desc.enum_type.values_by_number[v].name for v in val
                 )
